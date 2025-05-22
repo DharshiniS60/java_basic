94f3214ff01b56ad0c94118f75ca7ac312879532
DharshiniS60 <dharshinidharshini12022004@gmail.com>
2025-05-22 21:01:22 +0530

    Add files via upload

---
+++ area.java
@@ -0,0 +1,16 @@
+package dailypractise;
+import java.util.*;
+public class area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("l=");
+		int l=sc.nextInt();
+		System.out.println("b=");
+		int b=sc.nextInt();
+		int area=l*b;
+		System.out.println("area "+area);
+	}
+
+}
