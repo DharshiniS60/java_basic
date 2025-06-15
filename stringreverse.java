b9c4af5903189b0cd47e84b53fbcb0c1a2c37947
DharshiniS60 <dharshinidharshini12022004@gmail.com>
2025-06-15 18:58:59 +0530

    Add files via upload

---
+++ stringreverse.java
@@ -0,0 +1,17 @@
+package dailypractise;
+import java.util.*;
+
+public class stringreverse {
+	public static void main(String args[]) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter");
+	String input=sc.nextLine();
+	
+	String reversed="";
+	for(int i=input.length()-1;i>=0;i--) {
+		reversed+=input.charAt(i);
+	}
+	System.out.println("revrsed"+reversed);
+}
+}
+
